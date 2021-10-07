2567b2bd4d42e5828c042fad9e3016a9575b86ca
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-10-07 15:25:03 -0700

    initial commit

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,55 +8,55 @@ const client = require('../lib/client');
 
 describe('app routes', () => {
   describe('routes', () => {
-    let token;
+    // let token;
   
-    beforeAll(async () => {
-      execSync('npm run setup-db');
+    // beforeAll(async () => {
+    //   execSync('npm run setup-db');
   
-      await client.connect();
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'jon@user.com',
-          password: '1234'
-        });
+    //   await client.connect();
+    //   const signInData = await fakeRequest(app)
+    //     .post('/auth/signup')
+    //     .send({
+    //       email: 'jon@user.com',
+    //       password: '1234'
+    //     });
       
-      token = signInData.body.token; // eslint-disable-line
-    }, 10000);
+    //   token = signInData.body.token; // eslint-disable-line
+    // }, 10000);
   
-    afterAll(done => {
-      return client.end(done);
-    });
+    // afterAll(done => {
+    //   return client.end(done);
+    // });
 
-    test('returns animals', async() => {
+    test('returns location data', async() => {
 
-      const expectation = [
+      const expectation =
         {
-          'id': 1,
-          'name': 'bessie',
-          'cool_factor': 3,
-          'owner_id': 1
-        },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'cool_factor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'cool_factor': 10,
-          'owner_id': 1
-        }
-      ];
+          'formatted_query': expect.any(String),
+          'latitude': expect.any(String),
+          'longitude': expect.any(String)
+        };
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/location')
         .expect('Content-Type', /json/)
         .expect(200);
 
       expect(data.body).toEqual(expectation);
+    });
+
+    test('returns weather data', async() => {
+
+      const expectation = Array(7).fill({ 
+        'forecast': expect.any(String),
+        'time': expect.any(String) });
+
+      const data = await fakeRequest(app)
+        .get('/weather')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expect.arrayContaining(expectation));
     });
   });
 });
