be5c177733c9a3a73c7fb0bf67b1389ff42bf13d
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-10-08 09:48:27 -0700

    munge weather test passing

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,16 +1,27 @@
 const { mungeLocationFetch, mungeWeatherFetch } = require('../lib/utils.js');
 const { location_data } = require('../data/location-raw.js');
+const { weather_data } = require('../data/weather-raw.js');
 
 describe('utils', () => {
 
   test('munge location data', async() => {
     const expectation = {
-      'latitude': '33.7489924',
-      'longitude': '-84.3902644',
-      'formatted_query': 'Atlanta, Fulton County, Georgia, USA'
+      'latitude': expect.any(String),
+      'longitude': expect.any(String),
+      'formatted_query': expect.any(String)
     };
     
     const mungedData = mungeLocationFetch(location_data);
     expect(mungedData).toEqual(expectation);
+  });
+
+  test('munge weather data', async() => {
+    const expectation = {
+      'forecast': expect.any(String),
+      'time': expect.any(String),
+    };
+    
+    const mungedData = mungeWeatherFetch(weather_data);
+    expect(mungedData[0]).toEqual(expectation);
   });
 });
